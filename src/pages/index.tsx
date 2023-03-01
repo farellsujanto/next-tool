f96ee1370819bf323e755bf53b371c001ede6412
farells <farell.sujanto@flip.id>
2023-03-01 11:35:39 +0700

    Add console logs

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,9 +3,13 @@ import React from 'react';
 export default function Home() {
 
     function onDownloadButtonClick() {
+        console.log("DOWNLOAD BLOB")
         var data = new Blob(['res'], {type: 'text/csv'});
+        console.log(data)
         var csvURL = window.URL.createObjectURL(data);
+        console.log(csvURL)
         var tempLink = document.createElement('a');
+        console.log(tempLink)
         tempLink.href = csvURL;
         tempLink.setAttribute('download', 'filename.png');
         tempLink.click();
