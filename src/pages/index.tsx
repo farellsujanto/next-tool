ec2bbda1eeb9782e88637dc0b83edf4e5102fc16
farells <farell.sujanto@flip.id>
2023-03-01 13:11:02 +0700

    Test download

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,34 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 export default function Home() {
 
+    const [filedownloadlink, setFiledownloadlink] = useState("")
+
+    useEffect(() => {
+        var contacts = [{
+            name: 'Bob',
+            lastname: 'Smith'
+        }, {
+            name: 'James',
+            lastname: 'David'
+        }, {
+            name: 'Robert',
+            lastname: 'Miller'
+        }, {
+            name: 'David',
+            lastname: 'Martin'
+        }];
+
+        var myURL = window.URL || window.webkitURL //window.webkitURL works in Chrome and window.URL works in Firefox
+        var csv = JSON.stringify(contacts);
+        var blob = new Blob([csv], { type: 'text/csv' });
+        var csvUrl = myURL.createObjectURL(blob);
+        setFiledownloadlink(csvUrl);
+    }, []);
+
     function onDownloadButtonClick() {
         console.log("DOWNLOAD BLOB")
-        var data = new Blob(['res'], {type: 'text/csv'});
+        var data = new Blob(['res'], { type: 'text/csv' });
         console.log(data)
         var csvURL = window.URL.createObjectURL(data);
         console.log(csvURL)
@@ -17,8 +41,11 @@ export default function Home() {
 
     return (
         <section>
-            <button onClick={onDownloadButtonClick}>DOWNLOAD</button>
+            <button onClick={onDownloadButtonClick}>DOWNLOAD BLOB</button>
             <p>ASDASDAS</p>
+            <a download="next.svg" href={'/next.svg'}>Download1</a>
+            <p>ASDASDAS</p>
+            <a download="test.txt" href={filedownloadlink}>Download2</a>
         </section>
     )
 }
